137e0aecad4d0f48e5467ee589dccc5f046409df
bud aminof <budaminof@gmail.com>
2016-04-28 18:20:08 -0600

    after inital commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var app = angular.module('myApp', ['ngAnimate']);
+
+app.controller('main', function ($scope) {
+  $scope.vm = {};
+
+  $scope.data = [
+    {
+      title:  'Aspen',
+      image: 'http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Bud Anin",
+      date: new Date(),
+      votes: 0,
+      comments: []
+    },
+    {
+      title:  'Boulder',
+      image: 'http://im.rediff.com/news/2016/mar/01smith1.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Nick MacIntyre",
+      date: new Date(),
+      votes: 0,
+      comments: []
+    },
+    {
+      title:  'Tel Aviv',
+      image: 'http://www.planwallpaper.com/static/images/beautiful-sunset-images-196063.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Coutrney and Mike",
+      date: new Date(),
+      votes: 0,
+      comments: []
+    }
+  ];
+
+})
