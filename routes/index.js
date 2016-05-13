89faeaca265df08eb37ba1819984e0d2f921b12c
bud aminof <budaminof@gmail.com>
2016-05-12 18:50:08 -0600

    showing authors and comments and create at of comments and it shows!

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,41 @@ var router = express.Router();
 var knex = require('knex')(require('../knexfile')[process.env.DB_ENV || 'development']);
 
 router.get('/api/v1/posts', function(req, res, next) {
-  knex('posts')
+  var _posts = [];
+
+   knex('posts')
   .innerJoin('users', 'posts.user_id', 'users.user_id')
-  .returning('title', 'description', 'votes', 'image_url', 'create_at', 'username')
+  .select('posts.title', 'posts.description', 'posts.votes', 'posts.image_url', 'posts.create_at', 'users.username', 'posts.post_id')
   .then(function(data){
-    return res.json(data);
-  })
+
+    data.forEach(function (item){
+      _posts.push({
+        id: item.post_id,
+        title: item.title,
+        description: item.description,
+        image_url: item.image_url,
+        votes: item.votes,
+        username: item.username,
+        comments: []
+      })
+    })
+
+
+    return knex('comments')
+    .innerJoin('users', 'comments.user_id', 'users.user_id')
+    .select('users.username', 'comments.comment', 'comments.post_id', 'comments.create_at')
+    })
+    .then(function (dataComments) {
+
+      _posts.forEach(function (postItem){
+        dataComments.forEach(function (commentItem){
+          if(postItem.id === commentItem.post_id){
+            postItem.comments.push(commentItem)
+          }
+        })
+      })
+      res.json(_posts);
+    })
 
 });
 
