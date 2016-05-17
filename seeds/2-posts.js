6a3420ac3b6fa0f178fc4d3e6d9a8be764169d1f
bud aminof <budaminof@gmail.com>
2016-05-16 18:11:45 -0600

    seed with hispter filler text

--- seeds/2-posts.js
+++ seeds/2-posts.js
@@ -5,66 +5,59 @@ exports.seed = function(knex, Promise) {
 
     knex('posts')
      .insert({
-       title:  'Longborading in the California',
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+       title:  'Longborading in California',
+       description: "Fap biodiesel polaroid, art party meditation jean shorts chia. Cred slow-carb meh cardigan, small batch selvage aesthetic banjo organic drinking vinegar green juice echo park XOXO chartreuse. Jean shorts kogi godard banjo sartorial, sustainable butcher chicharrones brunch.",
        votes: 23,
        image_url: 'https://s-media-cache-ak0.pinimg.com/736x/c3/1c/5b/c31c5bba4051bb8dc018b9244dec17e8.jpg',
        user_id: 1
-      //  created_at: new Date("April 3, 2016")
      }),
    knex('posts')
      .insert({
        title:  'San Diego Longboarding',
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+       description: "Fap biodiesel polaroid, art party meditation jean shorts chia. Cred slow-carb meh cardigan, small batch selvage aesthetic banjo organic drinking vinegar green juice echo park XOXO chartreuse. Jean shorts kogi godard banjo sartorial, sustainable butcher chicharrones brunch. Food truck celiac put a bird on it, tofu brooklyn everyday carry next level fashion axe seitan taxidermy fap chambray raw denim. ",
        votes: 10,
        image_url: 'http://indosurflife.com/wp-content/uploads/2012/04/LongboardSurfGirl.jpg',
        user_id: 2
-      //  created_at: new Date("March 25, 2016")
      }),
    knex('posts')
      .insert({
        title:  "La Punta",
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+       description: "Fap biodiesel polaroid, art party meditation jean shorts chia. Cred slow-carb meh cardigan, small batch selvage aesthetic banjo organic drinking vinegar green juice echo park XOXO chartreuse.",
        votes: 13,
        image_url: 'http://www.surfertoday.com/images/stories/playacoco.jpg',
        user_id: 3
-      //  created_at: new Date("March 24, 2016")
      }),
    knex('posts')
      .insert({
        title:  'Bali',
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
-       votes: 35,
+       description: "Fap biodiesel polaroid, art party meditation jean shorts chia. Cred slow-carb meh cardigan, small batch selvage aesthetic banjo organic drinking vinegar green juice echo park XOXO chartreuse. Jean shorts kogi godard banjo sartorial, sustainable butcher chicharrones brunch. Food truck celiac put a bird on it, tofu brooklyn everyday carry next level fashion axe seitan taxidermy fap chambray raw denim.",
+       votes: 26,
        image_url: 'http://www.surfertoday.com/images/stories/surfingmaldives2.jpg',
        user_id: 1
-      //  created_at: new Date("March 02, 2016")
      }),
    knex('posts')
      .insert({
        title:  'There is a spot next to Google offices..',
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+       description: "Fap biodiesel polaroid, art party meditation jean shorts chia. Cred slow-carb meh cardigan, small batch selvage aesthetic banjo organic drinking vinegar green juice echo park XOXO chartreuse.",
        votes: 10,
        image_url: 'http://www.surfingmagazine.com/files/2010/03/line-up-545x371.jpg',
        user_id: 2
-      //  created_at: new Date("May 5, 2016")
      }),
    knex('posts')
      .insert({
        title:  'find your true north',
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+       description: "Fap biodiesel polaroid, art party meditation jean shorts chia. Cred slow-carb meh cardigan, small batch selvage aesthetic banjo organic drinking vinegar green juice echo park XOXO chartreuse. Jean shorts kogi godard banjo sartorial, sustainable butcher chicharrones brunch.",
        votes: 4,
        image_url: 'http://www.yeity.com/Photo%20Essays/Surfing%20Destinations/Lofotan1.jpg',
        user_id: 3,
-      //  created_at: new Date("March 9, 2016")
      }),
    knex('posts')
      .insert({
        title:  'Costa Rica',
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+       description: "Fap biodiesel polaroid, art party meditation jean shorts chia. Cred slow-carb meh cardigan, small batch selvage aesthetic banjo organic drinking vinegar green juice echo park XOXO chartreuse. Jean shorts kogi godard banjo sartorial, sustainable butcher chicharrones brunch. Food truck celiac put a bird on it, tofu brooklyn everyday carry next level fashion axe seitan taxidermy fap chambray raw denim. ",
        votes: 13,
        image_url: 'http://surfspots.ie/images/homebanner2.jpg',
        user_id: 2
-      //  created_at: new Date("February 26, 2016")
      })
 
   );
